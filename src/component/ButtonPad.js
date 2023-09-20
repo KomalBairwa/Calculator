b625ed6911714e22d7fbad64ea1f594d8978d5ee
KomalBairwa <bairwa.komal2002@gmail.com>
2023-09-20 22:42:32 +0530

    calculator

---
+++ src/component/ButtonPad.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './ButtonPad.css';
+export default function ButtonPad() {
+    
+    
+  return (
+     
+
+    <div className='button-pad'>
+        
+        <button>1</button>
+        <button>2</button>
+        <button>3</button><br/>
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button><br/>
+
+        <button>7</button>
+        <button>8</button>
+        <button>9</button><br/>
+
+        <button>+</button>
+        <button>0</button>
+        <button>-</button>
+        <button>=</button><br/>     
+
+    </div>
+  )
+}
---
+++ src/component/Label.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Label.css';
+import './ButtonPad.js';
+export default function Label(props) {
+  return (
+    <div className='display-area'>
+          <label>calculations will appear here!</label>
+    </div>
+  )
+}
